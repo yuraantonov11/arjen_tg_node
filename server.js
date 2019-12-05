cbfa560914282208850858f3aa9551450eaca2cb
yuraantonov11 <yuraantonov11@gmail.com>
2019-12-05 16:48:14 +0200

    add express

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const expressApp = express();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 expressApp.use(bot.webhookCallback('/secret-path'));
-bot.telegram.setWebhook('https://server.tld:8443/secret-path');
+// bot.telegram.setWebhook('https://server.tld:8443/secret-path');
 
 expressApp.get('/', (req, res) => {
     res.send('Hello World!')
