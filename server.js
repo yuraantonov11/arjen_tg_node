c3024ed6d7beacb84ff7ffc801af3dc1c598f02e
yuraantonov11 <yuraantonov11@gmail.com>
2019-12-05 16:46:08 +0200

    add express

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const Telegraf = require('telegraf');
+const express = require('express');
+const expressApp = express();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => {
-    console.log('Welcome');
-    return ctx.reply('Welcome')
+expressApp.use(bot.webhookCallback('/secret-path'));
+bot.telegram.setWebhook('https://server.tld:8443/secret-path');
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!')
 });
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+
+expressApp.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+});
