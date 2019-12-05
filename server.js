ce41a532dbe8f97e51f75814bfc439f85b36b3bd
yuraantonov11 <yuraantonov11@gmail.com>
2019-12-05 16:35:14 +0200

    'Welcome'

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => {
+    console.log('Welcome');
+    return ctx.reply('Welcome')
+});
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
