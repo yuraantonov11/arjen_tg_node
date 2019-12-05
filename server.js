adec6989089454a7e41b6a579d679fef109d3871
yuraantonov11 <yuraantonov11@gmail.com>
2019-12-05 16:54:26 +0200

    add add bot functionality

--- server.js
+++ server.js
@@ -13,3 +13,9 @@ expressApp.get('/', (req, res) => {
 expressApp.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}!`)
 });
+
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
