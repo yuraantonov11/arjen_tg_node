38f47916690a8467b26366999d3f8a70ef2db535
yuraantonov11 <yuraantonov11@gmail.com>
2019-12-06 18:18:26 +0200

    add init

--- server.js
+++ server.js
@@ -32,3 +32,4 @@ bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
+bot.startPolling();
