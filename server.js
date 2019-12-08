b75b76888138ecdaa532e7af3f5c8ad5ebb84250
yuraantonov11 <yuraantonov11@gmail.com>
2019-12-08 12:58:05 +0200

    enable all

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-// const Telegraf = require('telegraf');
-// const TelegrafInlineMenu = require('telegraf-inline-menu');
-// const express = require('express');
-// const http = require('http');
-// const Scrapper = require('./lib/scrapper');
-//
-// const menu = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`);
-// const expressApp = express();
-//
-// // Scrapper.getPageHtml('http://google.com').then(data => console.log(data));
-//
-// const bot = new Telegraf(process.env.BOT_TOKEN);
-// // expressApp.use(bot.webhookCallback('/secret-path'));
-// // bot.telegram.setWebhook('https://server.tld:8443/secret-path');
-//
-// expressApp.get('/', (req, res) => {
-//     res.send('Hello World!')
-// });
-//
-// // prevent idle
-// setInterval(function() {
-//     http.get("http://arjen-tg-node.herokuapp.com");
-// }, 300000); // every 5 minutes (300000)
-//
-// expressApp.listen(process.env.PORT, () => {
-//     console.log(`Example app listening on port ${process.env.PORT}!`)
-// });
-//
-// bot.use(menu.init());
-// // bot.start((ctx) => ctx.reply('Welcome'));
-// // bot.help((ctx) => ctx.reply('Send me a sticker'));
-// // bot.on('sticker', (ctx) => ctx.reply('👍'));
-// // bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-// // bot.launch();
-// bot.startPolling();
+const Telegraf = require('telegraf');
+const TelegrafInlineMenu = require('telegraf-inline-menu');
+const express = require('express');
+const http = require('http');
+const Scrapper = require('./lib/scrapper');
+
+const menu = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`);
+const expressApp = express();
+
+// Scrapper.getPageHtml('http://google.com').then(data => console.log(data));
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+// expressApp.use(bot.webhookCallback('/secret-path'));
+// bot.telegram.setWebhook('https://server.tld:8443/secret-path');
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+// prevent idle
+setInterval(function() {
+    http.get("http://arjen-tg-node.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
+expressApp.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}!`)
+});
+
+bot.use(menu.init());
+// bot.start((ctx) => ctx.reply('Welcome'));
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+// bot.on('sticker', (ctx) => ctx.reply('👍'));
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+// bot.launch();
+bot.startPolling();
